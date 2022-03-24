517fc7b0b149f178f4744087f44bd42c64a006ba
Focu5nik <80908739+Focu5nik@users.noreply.github.com>
2022-03-24 19:50:44 +0000

    Main function changed, main.c modified

--- src/main.c
+++ src/main.c
@@ -15,6 +15,18 @@ int NumberOfChar(char* str, char ch)
 }
 
 int main(int args, char** argv)
-{
-  
+{  
+    if (args < 2) {
+        printf("Invalid number of arguments");
+        return 1;
+    }
+
+    char str[MAX];
+    strncpy(str, argv[1], MAX);
+
+    printf("Enter the letter: ");
+    char ch = getchar();
+
+    printf("\nResult of the function: %u",   
+    NumberOfChar(str, ch));
 }
